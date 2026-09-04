aea39831cbb2b54ecd6324588623f5ba3bdbc55d
ypsitau <ypsitau@nifty.com>
2026-09-04 17:21:06 +0900

    implementing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+mod win_key_codes;
+
 pub struct Context {
 }
 
@@ -7,8 +9,16 @@ impl Context {
     pub fn new() -> Self {
         Context {}
     }
+    pub fn feed(&mut self, key: u8) -> Option<i32>{
+        // Handle the key input here
+        None
+    }
 }
 
+fn sub() {
+    win_key_codes::VK_A;
+
+}
 
 #[test]
 fn it_works() {
